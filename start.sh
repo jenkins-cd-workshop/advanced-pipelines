3a702fd42c43ffd4e7db4a6ef9f870325e3e51a2
Giorgio Sironi <g.sironi@elifesciences.org>
2017-06-04 12:33:10 +0100

    Builds making use of a precious-resource

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+what=${1}
+echo "Simulating startup of $what"
+sleep 5
+echo "$what started!"
---
+++ stop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+what=${1}
+echo "Simulating stopping of $what"
+sleep 5
+echo "$what stopped!"
